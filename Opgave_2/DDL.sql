f16e799f08e585e8623e84a8e0613c003b99f867
Villads Valur Korsholm Nielsen <villadsvalur@gmail.com>
2016-09-13 15:18:18 +0200

    REFERENCES fejl allover er lige blevet fixet

--- Opgave_2/DDL.sql
+++ Opgave_2/DDL.sql
@@ -1,38 +1,92 @@
-CREATE TABLE IF NOT EXISTS Student(
-    Student_id INT NOT NULL,
-    Student_Name VARCHAR(30) UNIQUE
+DROP TABLE IF EXISTS Student, Course, Project, Exam, Teacher, Supervisor, HandIn, CoursesHasHandIns, SupervisorSupervisesProject, StudentDoesProject, TeacherTeachesCourse;
 
+CREATE Table Student(
+    id INT ,
+    Student_Name VARCHAR(30) UNIQUE,
+
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS Course(
-    Course_id INT NOT NULL,
-    Course_Name VARCHAR(50)
+CREATE Table Course(
+    id INT ,
+    Course_Name VARCHAR(60),
 
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS Project(
-    Project_Name VARCHAR(30)
+CREATE Table Project(
+    Name VARCHAR(30),
 
+    PRIMARY KEY(Name)
 );
 
-CREATE TABLE IF NOT EXISTS Exam(
-    Exam_id INT NOT NULL
+CREATE Table Exam(
+    id INT ,
+    Student_id INT,
+    Attempts INT,
+    Grade VARCHAR(3),
 
+    PRIMARY KEY(id, Grade),
+    FOREIGN KEY(Student_id) REFERENCES Student(id)
 );
 
-CREATE TABLE IF NOT EXISTS HandIn(
-    HandIn_Name VARCHAR(30) NOT NULL,
-    APPROVED VARCHAR(3)
+CREATE Table HandIn(
+    Student_id INT,
+    HandIn_Name VARCHAR(30) ,
+    Approved VARCHAR(3),
 
+    PRIMARY KEY(Approved),
+    PRIMARY KEY(HandIn_Name),
+    FOREIGN KEY(Student_id) REFERENCES Student(id)
 );
 
-CREATE TABLE IF NOT EXISTS Teacher(
-    Teacher_id INT NOT NULL,
+CREATE Table Teacher(
+    id INT,
     Teacher_Name VARCHAR(30) UNIQUE,
-    PRIMARY KEY (Teacher_id)
+
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Supervisor(
-    Teacher_id INT NOT NULL REFERENCES Teacher(Teacher_id)
+CREATE Table Supervisor(
+    Teacher_id INT,
 
+    FOREIGN KEY (Teacher_id) REFERENCES Teacher(id)
+);
+
+
+/*
+Herunder findes alle relationerne vi har defineret i vores model
+*/
+CREATE Table StudentDoesProject(
+    Student_id INT,
+    Project_Name VARCHAR(60),
+    Project_Grade INT,
+
+    FOREIGN KEY(Student_id) REFERENCES Student(id),
+    FOREIGN KEY Project_Name REFERENCES Project(Name)
+);
+
+
+CREATE Table TeacherTeachesCourse(
+    Teacher_id INT,
+    Course_id INT,
+
+    FOREIGN KEY(Teacher_id) REFERENCES Teacher(id),
+    FOREIGN KEY (Course_id) REFERENCES Course(id)
+);
+
+CREATE Table SupervisorSupervisesProject(
+    Teacher_id INT,
+    Project_Name VARCHAR(60),
+
+    FOREIGN KEY(Teacher_id) REFERENCES Teacher(id),
+    FOREIGN KEY (Project_Name) REFERENCES Project(Name)
+);
+
+CREATE Table CoursesHasHandIns(
+    Course_id INT,
+    HandIn_Name VARCHAR(30),
+
+    FOREIGN KEY (Course_id) REFERENCES Course(id),
+    FOREIGN KEY (HandIn_Name) REFERENCES HandIn(Name)
 );
