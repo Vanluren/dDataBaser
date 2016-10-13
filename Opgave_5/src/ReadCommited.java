0ebbc3843887f330b7675e6fb03f4fc9393058c7
Villads Valur Korsholm Nielsen <villadsvalur@gmail.com>
2016-10-13 17:36:04 +0200

    Tilføjede lidt indmad til java-filerne

--- Opgave_5/src/ReadCommited.java
+++ Opgave_5/src/ReadCommited.java
@@ -6,20 +6,24 @@ import java.util.*;
 
 public class ReadCommited {
     public static void main(String args[]){
-        Connection connection = null;
-        Properties info = new Properties( );
-        info.put( "user", "root" );
-        info.put( "password", "root" );
-
-
-
+        Connection connection;
         try {
-            String url = "jdbc:mysql://localhost:8889";
-            connection = DriverManager.getConnection(url, info);
+            String url = "jdbc:mysql://localhost:8889/rooms_participants";
+            String userId = "root";
+            String password = "root";
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            connection = DriverManager.getConnection(url, userId,password);
             connection.setAutoCommit(false);
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 
-
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Rooms");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+                statement.close();
+                connection.close();
+            }
 
 
         }catch (Exception e){e.printStackTrace();}
--- Opgave_5/src/ReadUnCommited.java
+++ Opgave_5/src/ReadUnCommited.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 /**
@@ -7,20 +9,24 @@ import java.util.Properties;
  */
 public class ReadUnCommited {
     public static void main(String args[]){
-        Connection connection = null;
-        Properties info = new Properties( );
-        info.put( "user", "root" );
-        info.put( "password", "root" );
-
-
-
+        Connection connection;
         try {
             String url = "jdbc:mysql://localhost:8889";
-            connection = DriverManager.getConnection(url, info);
+            String userId = "root";
+            String password = "root";
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            connection = DriverManager.getConnection(url, userId,password);
             connection.setAutoCommit(false);
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
 
-
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Rooms");
+                while (resultSet.next()){
+                    System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+                    statement.close();
+                    connection.close();
+                }
 
 
         }catch (Exception e){e.printStackTrace();}
