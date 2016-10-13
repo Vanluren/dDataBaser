ab00385a1db3668e4e63f864a937885e8cbfb2ab
Villads Valur Korsholm Nielsen <villadsvalur@gmail.com>
2016-10-13 16:45:28 +0200

    Initial commit

---
+++ Opgave_5/src/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by villadsvalur on 10/10/2016.
+ */
+import java.sql.*;
+
+public class MainClass {
+    public static void main(String args[]){
+        Connection connection;
+
+        try {
+            String url = "jdbc:mysql://localhost:5000";
+
+        }catch (Exception e){e.printStackTrace();}
+
+    }
+}
